86023f4732e034e30e4f2ddabf8117d082ab4a8a
Zakir Nuriiev <kazamov@gmail.com>
2025-03-06 11:58:24 +0200

    extract JWT_EXPIRES_IN to env

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,11 +1,13 @@
-import jwt from 'jsonwebtoken';
+import jwt, { type SignOptions } from 'jsonwebtoken';
 
 import { JwtUserPayload } from '@/schemas/authSchemas.js';
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, JWT_EXPIRES_IN } = process.env;
 
 export function createToken(payload: JwtUserPayload): string {
-    return jwt.sign(payload, JWT_SECRET as string, { expiresIn: '24h' });
+    return jwt.sign(payload, JWT_SECRET as string, {
+        expiresIn: JWT_EXPIRES_IN as SignOptions['expiresIn'],
+    });
 }
 
 type VerifyTokenResult = { data: JwtUserPayload; error: null } | { data: null; error: Error };
